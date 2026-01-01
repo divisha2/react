a317437ad10d04f98ba586870760ccb530df1da2
Divisha <divisha.goel.24cse@bmu.edu.in>
2026-01-01 19:18:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, error!</p>
+    </div>
+  )
+}
+
+export default App
